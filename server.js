5bb8edf7beae32b36dd29ae7c358f0cb6c1885f0
Anjali Verma <verla003renu@gmail.com>
2026-06-20 22:49:12 +0530

    this is the backend

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+
+app.use(express.json());
+app.use(cors());
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.get("/",(req,res)=>{
+   res.send('<h1>Hii</h1>');
+})
+
+app.use("",authRoutes);
+
+const PORT =5001;
+app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`);
+});
+
